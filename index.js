7f8c683c9c2f3b39f2542c227feb9a9800b9c06a
Virginia Ladd <virginialadd2@gmail.com>
2020-08-03 15:02:10 -0600

    Add modules to application

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var custom = require('./hello.js');
 
 console.log(custom.sayHello);
 // Hello From my Custom Module!
+
+var parser = require('node-markdown');
+var html = parser.Markdown('Using markdown helps you focus on writing, not on markup');
+
+console.log(html);
+// <p>Using markdown helps you focus on writing, not on markup</p>
