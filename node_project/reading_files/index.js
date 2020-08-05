b2109ac78db96f90f5c197e5937486289e4e4956
Virginia Ladd <virginialadd2@gmail.com>
2020-08-05 15:33:56 -0600

    Update file path

---
+++ node_project/reading_files/index.js
@@ -0,0 +1,20 @@
+var dataFile = require('./data.json');
+
+console.log(dataFile);
+// { name: 'Virginia Ladd', address: 'Broomfield, Colorado' }
+console.log(dataFile.name);
+// Virginia Ladd
+
+
+
+var fs = require('fs');
+// fs = file system Module
+
+fs.readFile('./other.txt','utf-8',(err, data) => {
+  console.log(data);
+  // name = Virginia Ladd, address = Broomfield
+});
+// format is: what file do you need,
+// what format is it in
+// and last what do you want to do with the file
+// in this case either an error or data with a callback function
