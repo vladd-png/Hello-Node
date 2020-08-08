3cc07a0a5fe2bab43f8f5a732b1122ba62af3a8f
Virginia Ladd <virginialadd2@gmail.com>
2020-08-08 10:35:48 -0600

    Create public folder and index html and write html code for connect server

--- connect/index.js
+++ connect/index.js
@@ -1,2 +1,6 @@
 var connect = require("connect");
 var serveStatic = require("serve-static");
+
+var app = connect().use(serveStatic('public')).use(function(req, res) {
+  res.end('Not Found');
+}).listen(3000);
