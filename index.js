8578a821ac49352185f7a6d4b1f68c8b535753bc
Virginia Ladd <virginialadd2@gmail.com>
2020-08-08 10:26:51 -0600

    Create connect folder

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ var html = parser.Markdown('Using markdown helps you focus on writing, not on ma
 
 console.log(html);
 // <p>Using markdown helps you focus on writing, not on markup</p>
+
+var boldText = parser.Markdown('This text will be **bold**');
+console.log(boldText);
+// <p>This text will be <strong>bold</strong></p>
+
+var italicsText = parser.Markdown('This text will have *emphasis*');
+console.log(italicsText);
+<p>This text will have <em>emphasis</em></p>
