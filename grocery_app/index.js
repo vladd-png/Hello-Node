7a11779b2292b2917008cba1df7a3f4305a1ec98
Virginia Ladd <virginialadd2@gmail.com>
2020-08-18 10:11:16 -0600

    Store data as a variable to pass dynamically

--- grocery_app/index.js
+++ grocery_app/index.js
@@ -3,9 +3,13 @@ var app = express();
 // this creates the server for us
 app.use(express.static(__dirname));
 
+var allItems = [
+  {"item": "cake"}, {"item": "coffee"}
+]
+
 app.get('/items', (request, response) => {
-  response.send({"item":"eggs"});
-})
+  response.send(allItems);
+});
 
 var server = app.listen(3000, () => {
   console.log("server running at http://localhost:"+server.address().port)
