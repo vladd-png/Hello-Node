a83dfc9e5f9a90b92dfca54b0ef64dbac27c5fed
Virginia Ladd <virginialadd2@gmail.com>
2020-08-18 10:07:43 -0600

    Create a server data piece to hit on localhost

--- grocery_app/index.js
+++ grocery_app/index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
 // this creates the server for us
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+
+app.get('/items', (request, response) => {
+  response.send({"item":"eggs"});
+})
 
 var server = app.listen(3000, () => {
   console.log("server running at http://localhost:"+server.address().port)
