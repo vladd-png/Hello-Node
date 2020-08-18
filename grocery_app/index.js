3dc3afc6e3c5823453af44900d5fe0c29f50ccce
Virginia Ladd <virginialadd2@gmail.com>
2020-08-18 12:10:59 -0600

    Update server files to run with socket.io and add callback function for connections

--- grocery_app/index.js
+++ grocery_app/index.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
 var app = express();
 // this creates the server for us
+var http = require("http").Server(app);
+// creates http that can run server for App
+var socketio = require("socket.io")(http);
+// how you set up socket.io
+
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
@@ -19,6 +25,14 @@ app.post('/items', (request, response) => {
   request.sendStatus(200);
 });
 
-var server = app.listen(3000, () => {
+// whenever someone new logs on - connection - recieve a notification
+socketio.on('connection', (socket) => {
+  console.log('User Is Connected');
+});
+
+// this line runs without socket.io
+// var server = app.listen(3000, () => {
+// this line runs with socket.io - change app to http
+var server = http.listen(3000, () => {
   console.log("server running at http://localhost:"+server.address().port)
 });
